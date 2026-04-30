1cdfe8a0eb586bc8b23a4cd34d963fb524229368
Akul Khandelwal <akulkh1208@gmail.com>
2026-04-30 18:04:42 +0530

    half

--- app/Http/Controllers/Usercontroller.php
+++ app/Http/Controllers/Usercontroller.php
@@ -3,10 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Category;
 
 class Usercontroller extends Controller
 {
     function welcome (){
+        $Category=Category::get();
         return view('welcome');
     }
 }
